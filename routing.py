814e8e75fdfec70b86b76dc421a9075bd0e597be
Unknown <dder725@aucklanduni.ac.nz>
2019-06-03 22:34:21 +1200

    Changed the order of functions

--- routing.py
+++ routing.py
@@ -1,43 +1,4 @@
 # -*- coding: utf-8 -*-
-import json
-import os
-from pprint import pprint # "pretty print"
-filename = os.path.join('.', 'KuroseRoss5-16.json') # modify as required
-netjson = json.load(open(filename))
-pprint(netjson)
-
-import networkx as nx # saves typing later on
-graph = nx.Graph()
-graph.add_nodes_from((
- (node['id'], node['properties']) # node-attributes
- for node in netjson['nodes']))
-graph.add_edges_from((
- (link['source'], link['target'], {'cost': link['cost']}) # source-target-attributes
- for link in netjson['links']))
-for node, data in graph.nodes(data=True):
- pprint((node, data))
-for source, target, data in graph.edges(data=True):
-     pprint((source, target, data)) # edges & attributes
-for node in graph:
- pprint((node, dict(graph[node]))) # neighbours
-
-node_positions = nx.spring_layout(graph)
-edge_label_positions = nx.draw_networkx_edge_labels(
-        graph,
-        pos=node_positions,
-        node_labels=nx.get_node_attributes(graph, name='name'),
-        edge_labels=nx.get_edge_attributes(graph, name='cost'))
-nx.draw_networkx(graph, pos=node_positions)
-
-P,D = dijkstra_generalized(graph, source = 'u', weight='cost')
-
-def forwarding(predecessor, source):
-    """ 
-    Compute a forwarding table from a predecessor list. 
-    """
-    pass  # TODO
-    
-    
 def dijkstra_generalized(graph, source, weight='weight', 
                          infinity=None, 
                          plus=None,
@@ -77,4 +38,44 @@ def dijkstra_generalized(graph, source, weight='weight',
          if DvNew < D[v]:
              D[v] = DvNew
              P[v] = [w] #add a current node with its predecessor
- return D
+ return D, P
+
+import json
+import os
+from pprint import pprint # "pretty print"
+filename = os.path.join('.', 'KuroseRoss5-16.json') # modify as required
+netjson = json.load(open(filename))
+pprint(netjson)
+
+import networkx as nx # saves typing later on
+graph = nx.Graph()
+graph.add_nodes_from((
+ (node['id'], node['properties']) # node-attributes
+ for node in netjson['nodes']))
+graph.add_edges_from((
+ (link['source'], link['target'], {'cost': link['cost']}) # source-target-attributes
+ for link in netjson['links']))
+for node, data in graph.nodes(data=True):
+ pprint((node, data))
+for source, target, data in graph.edges(data=True):
+     pprint((source, target, data)) # edges & attributes
+for node in graph:
+ pprint((node, dict(graph[node]))) # neighbours
+
+node_positions = nx.spring_layout(graph)
+edge_label_positions = nx.draw_networkx_edge_labels(
+        graph,
+        pos=node_positions,
+        node_labels=nx.get_node_attributes(graph, name='name'),
+        edge_labels=nx.get_edge_attributes(graph, name='cost'))
+nx.draw_networkx(graph, pos=node_positions)
+
+D,P = dijkstra_generalized(graph, source = 'u', weight='cost')
+
+def forwarding(predecessor, source):
+    """ 
+    Compute a forwarding table from a predecessor list. 
+    """
+    pass  # TODO
+    
+    
