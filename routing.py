86201b5ab22d9a486f4cecebb2735ff388a01cef
Unknown <dder725@aucklanduni.ac.nz>
2019-06-03 21:57:33 +1200

    Added dijkstra alg

--- routing.py
+++ routing.py
@@ -28,7 +28,9 @@ edge_label_positions = nx.draw_networkx_edge_labels(
         node_labels=nx.get_node_attributes(graph, name='name'),
         edge_labels=nx.get_edge_attributes(graph, name='cost'))
 nx.draw_networkx(graph, pos=node_positions)
-pprint("x")
+
+P,D = dijkstra_generalized(graph, source = 'u', weight='cost')
+
 def forwarding(predecessor, source):
     """ 
     Compute a forwarding table from a predecessor list. 
@@ -41,7 +43,37 @@ def dijkstra_generalized(graph, source, weight='weight',
                          plus=None,
                          less=None,
                          min=None):
-    """
-    Least-cost or widest paths via Dijkstra's algorithm.
-    """
-    pass  # TODO: Please work from Lab 1 code
+ """
+ Least-cost or widest paths via Dijkstra's algorithm.
+ """
+ 
+ """
+ Shortest paths via Dijkstra's algorithm,
+ consistent with pseudocode on Slide 5-14.
+ """
+ import math
+ # Definitions consistent with Kurose & Ross
+ u = source
+ def c(x, y):
+     return graph[x][y][weight]
+ N = frozenset(graph.nodes())
+ NPrime = {u} # i.e. "set([u])"
+ D = dict.fromkeys(N, math.inf)
+ # Initialization
+ for v in N:
+     if graph.has_edge(u, v):
+         D[v] = c(u, v)
+         P[v] = [u] #add an entry to the predecessor dictionary
+         
+         D[u] = 0 # over-write inf entry for source
+ # Loop
+ while NPrime != N:
+     candidates = {w: D[w] for w in N if w not in NPrime}
+     w, Dw = min(candidates.items(), key=lambda item: item[1])
+     NPrime.add(w)
+ for v in graph[w]:
+     if v not in NPrime:
+         DvNew = D[w] + c(w, v)
+         if DvNew < D[v]:
+             D[v] = DvNew
+ return D
